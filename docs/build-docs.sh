60b34a9f1fdaaf5ed72daf9f3908564f6d848c90
Tapio Vierros <tapiovierros@gmail.com>
2012-02-07 23:03:30 +0200

    Added a script for building API documentation with Natural Docs.

---
+++ docs/build-docs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+# Builds documentation
+
+ND="naturaldocs"
+$ND -h >/dev/null 2>&1 || echo "Cannot find NaturalDocs!"
+
+DIR=$(dirname $(readlink -f $0))
+OUTDIR="$DIR/html"
+NDDIR="$DIR/NaturalDocs"
+mkdir -p "$OUTDIR" "$NDDIR"
+
+echo "Building API documentation..."
+$ND -i .. \
+	-o HTML "$OUTDIR" \
+	-p "$NDDIR" \
+	|| echo "API documentation building failed."
+
+echo "All done."
+
