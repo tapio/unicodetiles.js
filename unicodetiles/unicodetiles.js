5eda30bee793df69408eb2c74508497ec9e92d79
Tapio Vierros <tapiovierros@gmail.com>
2012-03-14 21:20:11 +0200

    Added clone method to Tile.

--- unicodetiles/unicodetiles.js
+++ unicodetiles/unicodetiles.js
@@ -63,47 +63,52 @@ ut.Tile = function(ch, r, g, b, br, bg, bb) {
 	/// Function: resetBackground
 	/// Clears the background color of this tile.
 	ut.Tile.prototype.resetBackground = function() { this.br = this.bg = this.bb = undefined; };
-	/// Function getColorHex
+	/// Function: getColorHex
 	/// Returns the hexadecimal representation of the color
 	ut.Tile.prototype.getColorHex = function() {
 		if (this.r !== undefined && this.g !== undefined && this.b !== undefined)
 			return "#" + this.r.toString(16) + this.g.toString(16) + this.b.toString(16);
 		else return "";
 	};
-	/// Function getBackgroundHex
+	/// Function: getBackgroundHex
 	/// Returns the hexadecimal representation of the background color
 	ut.Tile.prototype.getBackgroundHex = function() {
 		if (this.br !== undefined && this.bg !== undefined && this.bb !== undefined)
 			return "#" + this.br.toString(16) + this.bg.toString(16) + this.bb.toString(16);
 		else return "";
 	};
-	/// Function getColorRGB
+	/// Function: getColorRGB
 	/// Returns the CSS rgb(r,g,b) representation of the color
 	ut.Tile.prototype.getColorRGB = function() {
 		if (this.r !== undefined && this.g !== undefined && this.b !== undefined)
 			return 'rgb('+this.r+','+this.g+','+this.b+')';
 		else return "";
 	};
-	/// Function getBackgroundRGB
+	/// Function: getBackgroundRGB
 	/// Returns the CSS rgb(r,g,b) representation of the background color
 	ut.Tile.prototype.getBackgroundRGB = function() {
 		if (this.br !== undefined && this.bg !== undefined && this.bb !== undefined)
 			return 'rgb('+this.br+','+this.bg+','+this.bb+')';
 		else return "";
 	};
-	/// Function getColorJSON
+	/// Function: getColorJSON
 	/// Returns the JSON representation of the color, i.e. object { r, g, b }
 	ut.Tile.prototype.getColorJSON = function() {
 		if (this.r !== undefined && this.g !== undefined && this.b !== undefined)
 			return { "r": this.r, "g": this.g, "b": this.b };
 		else return {};
 	};
-	/// Function getBackgroundJSON
+	/// Function: getBackgroundJSON
 	/// Returns the JSON representation of the background color, i.e. object { r, g, b }
 	ut.Tile.prototype.getBackgroundJSON = function() {
 		if (this.r !== undefined && this.g !== undefined && this.b !== undefined)
 			return { "r": this.br, "g": this.bg, "b": this.bb };
 		else return {};
+	};
+	/// Function: clone
+	/// Returns a new copy of this tile
+	ut.Tile.prototype.clone = function() {
+		return new ut.Tile(this.ch, this.r, this.g, this.b, this.br, this.bg, this.bb);
 	};
 
 ut.NULLTILE = new ut.Tile();
